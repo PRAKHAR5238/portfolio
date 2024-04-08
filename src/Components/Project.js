78ea39dd0a63d2438ec052d86cacb9bd69bfd3f8
prakharshukla062 <prakharshukla062@gmail.com>
2024-04-09 00:08:28 +0530

    update file

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -20,6 +20,15 @@ export class Project extends Component {
             </div>
             <div className="col-md-4 col-sm-6 col-xs-12" data-aos="flip-right" data-aos-duration="1000">
               <div className="card">
+                <div className="cover item-f">
+                  <div className="card-back">
+                    <a href="https://www.figma.com/proto/FBc0RkUuHVF0oftiEQbZRB/Gate-Keeper-App?type=design&node-id=446-1879&t=J2YoBsIh0gSfmfRC-0&scaling=scale-down&page-id=0%3A1&starting-point-node-id=446%3A1879" target="_blank">Click here for demo</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-sm-6 col-xs-12" data-aos="flip-right" data-aos-duration="1000">
+              <div className="card">
                 <div className="cover item-b">
                   <div className="card-back">
                     <a href="#">Click here for demo</a>
